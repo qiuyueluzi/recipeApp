3d72629c1652c1a710eac964f3c804610f05351e
c0063vgw <c063vgw@yamaguchi-u.ac.jp>
2022-09-27 13:19:28 +0900

    材料タグファイル共有

--- make_csv/category.cpp
+++ make_csv/category.cpp
@@ -13,16 +13,19 @@ using std::endl;    using std::string;
 
 int main(int argc, char *argv[])
 {
-    int cnt = 0, i = 0;
+    int cnt = 0, i = 0, j = 0, k = 0;
     string str, file_name = argv[1];
-    vector<string> line;    //文字列読み込み用配列
+    vector<string> line, s_cate;    //文字列読み込み用配列
 
     stringstream path;  //ディレクトリのパスを指定
     path << "/home/hika4423/github/recipe/recipeApp/recipeApp/data_file/category/";
 
     //入出力ファイルを指定
     ifstream in_file(path.str() + file_name);
-    ofstream out_file(path.str() + file_name.erase(file_name.find('.')) + ".csv");
+    ofstream out_file1(path.str() + file_name.erase(file_name.find('.')) + "_l.csv");
+    ofstream out_file2(path.str() + file_name + "_s.csv");
+    ofstream out_file3(path.str() + "item_tag.csv");
+
 
     if(!in_file.is_open()){
         //ファイルオープン時のエラー処理
@@ -42,14 +45,38 @@ int main(int argc, char *argv[])
             line[cnt] = rm_bet(rm_char(line[cnt], ' '), "(", ")");
             line[cnt].erase(line[cnt].end()-1);
             line[cnt].erase(0, 8);
-            out_file << ++i << "," << line[cnt] << endl;
-            //cout << line[cnt] << endl;
+            out_file1 << ++i << "," << line[cnt] << endl;
+            //cout << name[j++] << endl;
+        }
+
+        if(line[cnt].find("text") != string::npos && line[cnt].find("品)") == string::npos && line[cnt].find("class") == string::npos){
+            line[cnt] = rm_char(line[cnt], ' ');
+            line[cnt].erase(line[cnt].end()-1);
+            line[cnt].erase(0, 8);
+            s_cate.push_back(line[cnt]);
         }
         cnt++;
     }
 
+    for(int n = 0; n < 31; n++){
+        s_cate.erase(s_cate.begin());
+    }
+
+    for(int n = 1196; n > 917; n--){
+        s_cate.pop_back();
+    }
+
+    for(auto n = s_cate.begin(); n != s_cate.end(); n)
+    {
+        if(j < 459) out_file2 << ++j << "," << *n++ << endl;
+        else out_file3 << ++k << "," << *n++ << endl;
+    }
+    
+
     in_file.close();
-    out_file.close();
+    out_file1.close();
+    out_file2.close();
+    out_file3.close();
 
     return 0;
 }
